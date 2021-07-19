256ebd708b7ac8519c428babeb068d15062f64a0
xingbow <wangxbzb@foxmail.com>
2021-07-19 11:49:04 +0800

    update: smbop for text2sql

--- backend/app/dataService/dataService.py
+++ backend/app/dataService/dataService.py
@@ -6,28 +6,29 @@ import sys
 
 try:
     import globalVariable as GV
+    import sqlParser as sp
 except ImportError:
     import app.dataService.globalVariable as GV
+    import app.dataService.sqlParser as sp
 
 class DataService(object):
     def __init__(self):
-        self.GV = GV
-        print('=================================================')
+        print("=== begin loading model ===")
+        self.sql_parser = sp.SmBop()
+        print("=== finish loading model ===")
         return
 
-    def initialization(self, data):
-        self.data = data
-        result = {'test': 'test'}
-        return result
-
-    def test(self):
-        print(self.GV.test)
+    def text2sql(self, q, db_id):
+        sql = self.sql_parser.predict(q, db_id)
+        return sql
 
 
 
 if __name__ == '__main__':
     print('start')
     dataService = DataService()
+    sql = dataService.text2sql("films and film prices that cost below 10 dollars", "cinema")
+    print("sql: {}".format(sql))
 
 
 
--- backend/app/dataService/globalVariable.py
+++ backend/app/dataService/globalVariable.py
@@ -7,4 +7,11 @@ test = os.getcwd()
 _current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # data folder
-DATA_FOLDER = os.path.join(_current_dir, '../data/')  
+DATA_FOLDER = os.path.join(_current_dir, '../data/')
+SPIDER_FOLDER = os.path.join(DATA_FOLDER, "dataset/spider")
+NVBENCH_FOLDER = os.path.join(DATA_FOLDER, "dataset/nvBench")
+
+# model folder
+MODEL_FOLDER = os.path.join(DATA_FOLDER, 'model')
+SMBOP_PATH = os.path.join(DATA_FOLDER, 'model/smbop.tar.gz')
+SMBOP_FOLDER = os.path.join(MODEL_FOLDER, "SmBop")

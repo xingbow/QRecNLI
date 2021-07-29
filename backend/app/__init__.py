11f57aed102eef222ba47ed7f8baddb57fe310d2
ChengFR <459763742@qq.com>
2021-07-29 16:18:00 +0800

    Update backend apis

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,10 +2,12 @@
 from flask import Flask
 from flask_cors import CORS
 from app.dataService.dataService import DataService
+from app.dataService.utils.helpers import NpEncoder
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.json_encoder = NpEncoder
 
 # flask_cors: Cross Origin Resource Sharing (CORS), making cross-origin AJAX possible.
 CORS(app)

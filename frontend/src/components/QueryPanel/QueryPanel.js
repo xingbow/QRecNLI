04cbcdfa7c57f580228c8371089a5d272c19223c
ChengFR <459763742@qq.com>
2021-08-12 18:35:28 +0800

    Reorganize front-end code structure

---
+++ frontend/src/components/QueryPanel/QueryPanel.js
@@ -0,0 +1,37 @@
+import dataService from '../../service/dataService.js'
+import pipeService from '../../service/pipeService.js'
+
+export default {
+    name: "QueryPanel",
+    data() {
+        return {
+            userText: "",
+        }
+    },
+    methods: {
+        search: function() {
+            if (this.userText.length > 0) {
+                let userText = this.userText;
+                dataService.text2SQL([this.userText, this.dbselected], (data) => {
+                    let sqlResult = {
+                            "sql": data["data"]["sql"].trim(),
+                            "data": data["data"]["data"],
+                            "nl": userText.trim()
+                        }
+                        // send "sql" to settings and record sql history
+                    if (sqlResult["sql"].length > 0) {
+                        dataService.SQL2text(sqlResult["sql"], this.dbselected, (data) => {
+                            sqlResult.explanation = data["data"];
+                            dataService.SQL2VL(sqlResult["sql"], this.dbselected, (data) => {
+                                sqlResult.vlSpecs = data["data"];
+                                pipeService.emitSQL(sqlResult);
+                            });
+                        });
+                    }
+                });
+            } else {
+                alert("input text is empty");
+            }
+        }
+    }
+}

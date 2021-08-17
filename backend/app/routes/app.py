0112625f385cf080d5d8840a49bf8bbdc522d50d
ChengFR <459763742@qq.com>
2021-08-17 23:32:46 +0800

    Update sql2vl function: resturn data when no vega-lite specifications can be found

--- backend/app/routes/app.py
+++ backend/app/routes/app.py
@@ -5,6 +5,7 @@ from app.routes.api import api
 from app.dataService.utils.helpers import NpEncoder
 from app.dataService.dataService import DataService
 
+
 def create_app():
     app = Flask(__name__)
 

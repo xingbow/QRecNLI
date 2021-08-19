b906c95b8144ef749b2203e145e1133c0502f1c0
ChengFR <459763742@qq.com>
2021-08-19 15:40:11 +0800

    Merge conficts and adjust setting code structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,7 +16,7 @@
       </div>
       <div class="d-flex flex-row rowele">
         <div class="p border-right" style="width: 25%; text-align:start;">
-          <Settings :tableLists="tableLists" :tables="tables"></Settings>
+          <Settings :tables="tables"></Settings>
         </div>
         <div class="p" style="width: 75%; text-align:start;">
           <ResultView :tables="tables" :querySugg="querySugg" />
@@ -49,7 +49,6 @@ export default {
         dbLists: [],
         dbInfo: [],
         tables: {},
-        tableLists: [],
         querySugg: {},
     }
   },
@@ -61,7 +60,6 @@ export default {
       if(dbselected){
           dataService.getTables(dbselected, (tableData)=>{
             _this.tables = tableData;
-            _this.tableLists = Object.keys(tableData);
           // get nl/sql suggestion from backend
           dataService.SQLSugg(dbselected, (suggData)=>{
             _this.querySugg = suggData
@@ -85,7 +83,6 @@ export default {
           if(dbselected.length>0){
             dataService.getTables(dbselected, (tableData)=>{
                   _this.tables = tableData;
-                  _this.tableLists = Object.keys(tableData);
                 // get nl/sql suggestion from backend
                 dataService.SQLSugg(dbselected, (suggData)=>{
                   _this.querySugg = suggData

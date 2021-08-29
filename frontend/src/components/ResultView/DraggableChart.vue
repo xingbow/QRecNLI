f191b65c37e33c18bb30bec7c06714c8f0c3462c
ChengFR <459763742@qq.com>
2021-08-30 01:01:06 +0800

    Create DraggableChart class and smooth the dragging interactions

---
+++ frontend/src/components/ResultView/DraggableChart.vue
@@ -0,0 +1,67 @@
+<template>
+  <vue-draggable-resizable
+    :w="w"
+    :h="h"
+    @resizing="onResize"
+  >
+    <div class="draggable-chart-container">
+      <el-row class="float-button-rol">
+        <el-button
+          plain
+          size="mini"
+          icon="el-icon-delete"
+          v-on:click="onDelete"
+        ></el-button>
+      </el-row>
+      <slot></slot>
+    </div>
+  </vue-draggable-resizable>
+</template>
+
+<script>
+/* global _ $*/
+import VueDraggableResizable from "vue-draggable-resizable";
+import "vue-draggable-resizable/dist/VueDraggableResizable.css";
+
+export default {
+  name: "DraggableChart",
+  components: { VueDraggableResizable },
+  props: {
+    onDelete: {
+      type: Function,
+      default: () => ""
+    },
+    onResize: {
+      type: Function,
+      default: () => true
+    },
+    w: Number,
+    h: Number,
+  },
+  methods: {
+      elementMouseDown: function(e){
+          console.log(e);
+          e.stopPropagation();
+      }
+  },
+};
+</script>
+
+<style scoped>
+.draggable-chart-container {
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+}
+
+.float-button-rol {
+  position: absolute;
+  /* top: -30px; */
+  top: 2px;
+  right: 2px;
+}
+
+.el-button--mini {
+  padding: 7px;
+}
+</style>

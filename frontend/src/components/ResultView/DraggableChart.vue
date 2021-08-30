34502caaff40b632ba9b1131287f87b895960388
ChengFR <459763742@qq.com>
2021-08-30 19:48:54 +0800

    Add widgets for charts and history items

--- frontend/src/components/ResultView/DraggableChart.vue
+++ frontend/src/components/ResultView/DraggableChart.vue
@@ -1,11 +1,26 @@
 <template>
   <vue-draggable-resizable
-    :w="w"
-    :h="h"
-    @resizing="onResize"
+    :w="w + margin.left + margin.right"
+    :h="h + margin.top + margin.bottom"
+    @resizing="_onResize"
   >
     <div class="draggable-chart-container">
-      <el-row class="float-button-rol">
+      <div v-if="titleVisible === true">
+        <span class="title">{{ title }}</span>
+      </div>
+      <el-row class="float-button-rol" v-if="inputVisible === false">
+        <el-button
+          plain
+          size="mini"
+          icon="el-icon-edit"
+          v-on:click="inputVisible = true"
+        ></el-button>
+        <el-button
+          plain
+          size="mini"
+          icon="el-icon-setting"
+          v-on:click="settingVisible = true"
+        ></el-button>
         <el-button
           plain
           size="mini"
@@ -13,6 +28,33 @@
           v-on:click="onDelete"
         ></el-button>
       </el-row>
+      <div class="title-input-container" v-if="inputVisible === true">
+        <el-input
+          v-model="title"
+          width="w"
+          class="title-input"
+          @blur="
+            inputVisible = false;
+            titleVisible = true;
+          "
+        ></el-input>
+      </div>
+      <el-popover
+        class="popover-setting"
+        placement="right"
+        width="100"
+        trigger="manual"
+        v-model="settingVisible"
+      >
+        <el-button
+          plain
+          size="mini"
+          type="text"
+          icon="el-icon-close"
+          class="popover-close"
+          v-on:click="settingVisible = false"
+        ></el-button>
+      </el-popover>
       <slot></slot>
     </div>
   </vue-draggable-resizable>
@@ -26,42 +68,108 @@ import "vue-draggable-resizable/dist/VueDraggableResizable.css";
 export default {
   name: "DraggableChart",
   components: { VueDraggableResizable },
+  data() {
+    return {
+      settingVisible: false,
+      inputVisible: false,
+      titleVisible: true,
+      title: "title",
+      margin: {
+        top: 10,
+        bottom: 10,
+        left: 10,
+        right: 10,
+      },
+    };
+  },
   props: {
     onDelete: {
       type: Function,
-      default: () => ""
+      default: () => "",
     },
     onResize: {
       type: Function,
-      default: () => true
+      default: () => true,
     },
-    w: Number,
-    h: Number,
+    w: {
+      type: Number,
+      default: 150,
+    },
+    h: {
+      type: Number,
+      default: 150,
+    },
+    defaultTitle: {
+      type: String,
+      default: "",
+    },
+  },
+  mounted() {
+      console.log(this.defaultTitle);
+    this.title = this.defaultTitle;
   },
   methods: {
-      elementMouseDown: function(e){
-          console.log(e);
-          e.stopPropagation();
-      }
+    _onResize: function (x, y, width, height) {
+      this.onResize(
+        x,
+        y,
+        width - this.margin.left - this.margin.right,
+        height - this.margin.top - this.margin.bottom
+      );
+    },
   },
 };
 </script>
 
 <style scoped>
 .draggable-chart-container {
-    overflow: hidden;
-    width: 100%;
-    height: 100%;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  padding: 10px;
+}
+
+.draggable-chart-container:hover .float-button-rol {
+  display: block;
 }
 
 .float-button-rol {
   position: absolute;
-  /* top: -30px; */
-  top: 2px;
-  right: 2px;
+  top: -45px;
+  right: -5px;
+  display: none;
+  padding: 10px;
 }
 
 .el-button--mini {
-  padding: 7px;
+  padding: 8px;
+}
+
+.popover-setting {
+  position: absolute;
+  top: -50px;
+  right: -10px;
+}
+
+.popover-close {
+  position: absolute;
+  right: 10px;
+  top: -3px;
+}
+
+.title-input-container {
+  /* margin: 0px 10px; */
+  width: calc(100% - 20px);
+  position: absolute;
+}
+
+.title-input {
+  top: -55px;
+}
+
+.title {
+  position: absolute;
+  top: -35px;
 }
 </style>
